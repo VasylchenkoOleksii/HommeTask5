1536a91cd9c203f5f799154417066634c6701250
vasylchenko <foxdesert1@rambler.ru>
2017-06-21 16:13:34 +0300

    111

---
+++ src/main/java/HommeTask5/Address.java
@@ -0,0 +1,98 @@
+package HommeTask5;
+
+import javax.persistence.*;
+
+/**
+ * Created by vasylchenko on 21.06.2017.
+ */
+@Entity
+@Table( name = "TMP_ADDRESS")
+public class Address {
+    @Id
+    @GeneratedValue (strategy = GenerationType.SEQUENCE, generator = "address_sequence")
+    @SequenceGenerator(name = "address_sequence",
+            sequenceName = "sequence_addr",
+            initialValue = 1,
+            allocationSize = 1)
+    @Column(name = "addressid")
+    private int id;
+    private String country;
+    private String postcode;
+    private String district;
+    private String city;
+    private String street;
+    private String houseno;
+    private String flat;
+
+
+    public Address() {
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseno() {
+        return houseno;
+    }
+
+    public void setHouseno(String houseno) {
+        this.houseno = houseno;
+    }
+
+    public String getFlat() {
+        return flat;
+    }
+
+    public void setFlat(String flat) {
+        this.flat = flat;
+    }
+
+
+}
--- src/main/java/HommeTask5/App.java
+++ src/main/java/HommeTask5/App.java
@@ -14,8 +14,16 @@ public class App
 {
     public static void main( String[] args )
     {   System.out.println( "Hello World!1" );
-        ContragentType contragenttype = new ContragentType("5K","hghghghgghg", 4);
+        ContragentType contragenttype = new ContragentType("19","hgdfhghghgghg", 4);
         Category category = new Category("OLE ole", new Date());
+        Address address = new Address();
+        address.setCountry("USA");
+        address.setCity("Chicago");
+        address.setDistrict("NBA");
+        address.setStreet("NiggaMan");
+        address.setHouseno("18");
+        address.setFlat("145");
+        address.setPostcode("61058");
         System.out.println( "Hello World!2" );
         EntityManagerFactory emf1 = Persistence.createEntityManagerFactory("TEST");
         EntityManager em1 = emf1.createEntityManager();
@@ -23,6 +31,7 @@ public class App
         tx1.begin();
         em1.persist(contragenttype);
         em1.persist(category);
+        em1.persist(address);
         tx1.commit();
         em1.close();
         emf1.close();
